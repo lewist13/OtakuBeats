721900b107196a27a9e1aa89412352674ff813af
lewist13 <lewistpro@gmail.com>
2023-05-09 12:00:55 -0400

    Initial Commit

---
+++ AniMusic/AniMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  AniMusicApp.swift
+//  AniMusic
+//
+//  Created by TaeVon Lewis on 5/9/23.
+//
+
+import SwiftUI
+
+@main
+struct AniMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
