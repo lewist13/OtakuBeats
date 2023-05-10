37d543e326fb3bed4732718a27ec4e33f99b0a01
lewist13 <lewistpro@gmail.com>
2023-05-10 17:45:40 -0400

    Initial Commit

---
+++ OtakuBeats/OtakuBeatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  OtakuBeatsApp.swift
+//  OtakuBeats
+//
+//  Created by TaeVon Lewis on 5/10/23.
+//
+
+import SwiftUI
+
+@main
+struct OtakuBeatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
