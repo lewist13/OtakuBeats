ce8690bd1bcc33cb9a87a958ff77fb256cfcd52e
taevonlewis <lewistpro@gmail.com>
2023-06-29 16:33:57 -0400

    modified launch screen to show links to each view

--- OtakuBeats/OtakuBeatsApp.swift
+++ OtakuBeats/OtakuBeatsApp.swift
@@ -16,13 +16,27 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     }
 }
 
+struct CombinedView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                NavigationLink("Go to ContentView", destination: ContentView())
+                NavigationLink("Go to Anime Fetch Example", destination: AnimeListView())
+                NavigationLink("Go to YouTube Example", destination: YouTubeExampleView())
+                NavigationLink("Go to Spotify Example", destination: SpotifyExampleView())
+                NavigationLink("Go to Apple Music Example", destination: AppleMusicExampleView())
+            }
+        }
+    }
+}
+
 @main
 struct OtakuBeatsApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CombinedView()
         }
     }
 }
